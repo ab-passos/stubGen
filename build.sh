6b91d320caaf8e9daf15e21b5bf8204c2800c75f
Andre Passos <andrebritopassos@gmail.com>
2016-07-06 20:44:32 +0200

    Changed build file

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash 
 
-ocamlopt -c -I /Users/andrepassos/.opam/system/lib/cil/ -I /Users/andrepassos/.opam/system/lib/cmdliner/ stubGen.ml
-ocamlopt -ccopt -L/Users/andrepassos/.opam/system/lib/cil/ -o main unix.cmxa str.cmxa nums.cmxa /Users/andrepassos/.opam/system/lib/cil/cil.cmxa /Users/andrepassos/.opam/system/lib/cmdliner/cmdliner.cmxa stubGen.cmx
-
-
-#ocamlopt -c -I /Users/andrepassos/.opam/system/lib/cmdliner/ rm.ml
-#ocamlopt -o rmml unix.cmxa str.cmxa nums.cmxa /Users/andrepassos/.opam/system/lib/cmdliner/cmdliner.cmxa rm.cmx
+ocamlfind ocamlopt -c -package cil,cmdliner stubGen.ml
+ocamlfind ocamlopt -o main -linkpkg -package cil,cmdliner stubGen.cmx
+ocamlfind ocamlc -o test -package oUnit -package cmdliner -package cil -linkpkg -g foo.ml stubGen.ml test_stubGen.ml
