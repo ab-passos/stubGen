3c0c92bdeb55cfce4fdba0f9179cd0c63521ea01
Andre Passos <andrebritopassos@gmail.com>
2016-07-09 21:12:42 +0200

    Using kapput framework instead of Ounit

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 ocamlfind ocamlopt -c -package cil,cmdliner stubGen.ml
 ocamlfind ocamlopt -o main.exe -linkpkg -package cil,cmdliner stubGen.cmx -g main.ml
-ocamlfind ocamlc -o test.exe -package oUnit -package cmdliner -package cil -linkpkg -g stubGen.ml test_stubGen.ml
+ocamlfind ocamlc -o test.exe -package cmdliner,cil,kaputt -linkpkg -g stubGen.ml test_stubGen.ml
--- test_stubGen.ml
+++ test_stubGen.ml
@@ -1,13 +1,54 @@
-open OUnit2;;
+open Kaputt.Abbreviations;;
 
-let test1 test_printCallbackType = assert_equal "cb_funct_t" (StubGen.printCallbackType "funct")
+let unity x = x;;
 
-(* Name the test cases and group them together *)
-let suite =
-"suite">:::
- ["test1">:: test1;]
+let t1 =
+	Test.make_simple_test
+    ~title:"first test"
+    (fun () -> Assert.equal_int 2 2)
 ;;
+
+let test_printCallbackType =
+	Test.make_simple_test
+	~title:"Print Callback Type test"
+	(fun () ->
+		(
+	   		List.iter (fun (x,y) -> Assert.equal_string x (StubGen.printCallbackType y))
+	   		["cb_type_t","type";
+	   		"cb_f_t","f";
+	   		"cb_some_type_t","some_type";
+	   		"cb_someType_t","someType"]
+		)
+	)
+
+let test_FileNameWithoutExtension = 
+	Test.make_simple_test
+	~title:"Print File name without extensions"
+	(fun () -> 
+		List.iter (fun (expected,input) -> 
+		Assert.equal_string expected (StubGen.getFileNameWithoutExtension input))
+		["test","test.h";
+	 	"a","a.h";
+	 	"myFile","myFile.c";]
+	)
+	
+;;
+
+(*
+let testFileNameWithoutPath test_FileNameWithoutPath =
+	List.iter (fun (expected,input) -> 
+		assert_equal expected (StubGen.getFileNameWithoutPath input))
+	["home/xxx/yyyy/zue/file.h","file.h";
+	 "home/xxx/yyyy/file.h","file.h";
+	 "home/xxx/file.h","file.h";
+	 "home/file.h","file.h"]
+;;
+*)
+
+
+
 
 let () =
-  run_test_tt_main suite
+  Test.run_tests [t1;test_printCallbackType; test_FileNameWithoutExtension]
 ;;
+
